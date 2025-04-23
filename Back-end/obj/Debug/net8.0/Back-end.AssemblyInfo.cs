977debfd4931cf0d02ec427e2d205f40b932fd13
bengayed <perf-analysis-mouhamed.bengayed86@outlook.com>
2025-04-23 14:20:46 +0100

    feat:modify file program

--- Back-end/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
+++ Back-end/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f95572a229b790959f604215841ce420336e2b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5c0b0b41a8114120543d6be229cbabbbb4f8ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
